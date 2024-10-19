7838c602bf9ec74e1bb2cf80d096696745374108
kenneth osborne <kbosborne1982@gmail.com>
2024-10-19 14:46:29 -0400

    fixed group page

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "pages/home.html")
+
+def group_view(request):
+    return render(request, "pages/groups.html")
